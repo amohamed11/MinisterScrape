e4060ea05b407d8af90a2d99e306aee53baacddc
Anas Mohamed <amohamed@ualberta.ca>
2018-05-30 07:50:07 -0600

    Added Party and Images to Database
    The database now has the Party of each member, and Image urls for profile picture.
    Updated README.md.

--- MinisterScrape.py
+++ MinisterScrape.py
@@ -1,21 +1,23 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import sqlite3
+import re
 
 def table_create(conn):
     c = conn.cursor()
 
     try:
         c.execute('''CREATE TABLE ministers
-               (name text, riding text, email text, phone text)''')
+               (name text, riding text, party text, email text, phone text, image_url text)''')
     except:
-        pass
+        c.execute(''' DROP TABLE ministers ''')
+        table_create(conn)
 
-def insert_minister(conn, name, riding, email, phone):
+def insert_minister(conn, name, riding, party, email, phone, image_url):
     c = conn.cursor()
 
-    query = '''INSERT INTO ministers VALUES(?, ?, ?, ?)''' 
-    c.execute(query, [name, riding, email, phone])
+    query = '''INSERT INTO ministers VALUES(?, ?, ?, ?, ?, ?)''' 
+    c.execute(query, [name, riding, party, email, phone, image_url])
 
 def main():
     f = urllib.request.urlopen('http://www.assembly.ab.ca/net/index.aspx?p=mla_report&memPhoto=True&alphaboth=True&alphaindex=True&build=y&caucus=All&conoffice=True&legoffice=True&mememail=True') 
@@ -30,28 +32,33 @@ def main():
     for entry in soup.find_all("div", class_="entry"):
         office = entry.find("div", class_="office")
         #Extracts the email and phone from the contact information
-        for child in office.contents:
-            if "Email:" in str(child):
-                email = child.get_text()
+        for contact in office.contents:
+            if "Email:" in str(contact):
+                email = contact.get_text()
                 email = email[6:]
 
-            if "Phone:" in str(child):
-                phone = child.get_text()
+            if "Phone:" in str(contact):
+                phone = contact.get_text()
                 phone = phone[6:]
+        
+        member = entry.find("div", class_="member")
+        bio = member.contents
+        riding = bio[len(bio)-3]
 
         try:  #Gets the rest of the info of the minister
             name = entry.find("b").get_text()
-            member = entry.find("div", class_="member")
-            bio = member.contents
-            riding = bio[len(bio)-3]
+            image = entry.find('img')
+            image_url = image['src']
+            image_url = 'http://www.assembly.ab.ca/' + str(image_url[3:])
+            party = str(bio[len(bio)-5])
+            party = re.sub('[()]', '', party)
         except:  #If there is an error with finding the name, then the position is vacant
             name = "Vacant"
-            member = entry.find("div", class_="member")
-            bio = member.contents
-            riding = bio[len(bio)-3]
             email = entry.find_next("a").get_text()
+            party = 'None'
+            image_url = 'None'
 
-        insert_minister(conn, str(name), str(riding), str(email), str(phone))
+        insert_minister(conn, str(name), str(riding), party, str(email), str(phone), str(image_url))
     
     #Commit all the Inserts and close the database
     conn.commit()
--- README.md
+++ README.md
@@ -9,5 +9,5 @@ sqlite3
 I made this to create a database of current ministers of Alberta for a future project of mine.
 The program creates a Ministers.db that has a information directly scraped from the MLA directory website.
 The database has the table "ministers" with the following Schema:
-CREATE TABLE ministers (name text, riding text, email text, phone text)
-![Screenshot](https://s7.postimg.cc/6vc8gnxxn/database_example.png)
+CREATE TABLE ministers (name text, riding text, party text, email text, phone text, image_url text)
+![Screenshot](https://s15.postimg.cc/5os3ubem3/Screenshot_from_2018-05-30_07.46.51.png)
